398dd21d3df4192c4fdea551f5c587de1cea3f30
Dennis Prasetia <dennis.prasetia@gmail.com>
2020-03-24 16:12:01 +0700

    Initial RHK as new library

--- rhk/src/main/java/com/wonokoyo/rhk/menu/RhkHomeFragment.java
+++ rhk/src/main/java/com/wonokoyo/rhk/menu/RhkHomeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -13,6 +14,10 @@ import android.view.ViewGroup;
 import com.wonokoyo.rhk.R;
 
 public class RhkHomeFragment extends Fragment {
+
+    private CardView cvCatatan;
+    private CardView cvLaporan;
+    private CardView cvKonfirmasi;
 
     public RhkHomeFragment() {
         // Required empty public constructor
@@ -27,6 +32,16 @@ public class RhkHomeFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+        cvCatatan = view.findViewById(R.id.cvCatatan);
+        cvCatatan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        cvLaporan = view.findViewById(R.id.cvLaporan);
+
+        cvKonfirmasi = view.findViewById(R.id.cvKonfirmasi);
     }
 }
